f7f173ea8498ed1bf0fcc9250e95822a1569fee5
Priydarshini Karanth <kpriya1203@gmail.com>
2024-06-30 20:42:26 +0530

    formdata api setup

---
+++ 01-FormDataAPI-React/src/App.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+
+function Register() {
+  const [isMember, setIsMember] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <section className="register-page full-page">
+      <form className="form" onSubmit={handleSubmit}>
+        <h3>{isMember ? 'login' : 'register'}</h3>
+        {/* name field */}
+        {!isMember && (
+          <div className="form-row">
+            <label htmlFor="name" className="form-label">
+              name
+            </label>
+            <input type="text" id="name" name="name" className="form-input" />
+          </div>
+        )}
+
+        {/* email field */}
+        <div className="form-row">
+          <label htmlFor="email" className="form-label">
+            email
+          </label>
+          <input type="email" id="email" name="email" className="form-input" />
+        </div>
+        {/* password field */}
+        <div className="form-row">
+          <label htmlFor="password" className="form-label">
+            password
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            className="form-input"
+          />
+        </div>
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+        <p>
+          {isMember ? 'Not a member yet? ' : 'Already a member?'}
+          <button
+            type="button"
+            className="member-btn"
+            onClick={() => setIsMember(!isMember)}
+          >
+            {isMember ? 'Register' : 'Login'}
+          </button>
+        </p>
+      </form>
+    </section>
+  );
+}
+
+export default Register;

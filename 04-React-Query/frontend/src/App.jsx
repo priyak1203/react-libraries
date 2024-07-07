7cd953407268c44f0a76568c653ad8b5ff101ecb
Priydarshini Karanth <kpriya1203@gmail.com>
2024-07-08 00:05:09 +0530

    frontend setup

--- 04-React-Query/frontend/src/App.jsx
+++ 04-React-Query/frontend/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
+import Form from './Form';
+import Items from './Items';
+
+const defaultItems = [
+  { id: nanoid(), title: 'walk the dog', isDone: false },
+  { id: nanoid(), title: 'wash dishes', isDone: false },
+  { id: nanoid(), title: 'drink coffee', isDone: true },
+  { id: nanoid(), title: 'take a nap', isDone: false },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [items, setItems] = useState(defaultItems);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <section className="section-center">
+      <Form />
+      <Items items={items} />
+    </section>
+  );
 }
 
-export default App
+export default App;

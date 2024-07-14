60e5f5a033d8000a3d785492fbe7639d6a7afbed
Priydarshini Karanth <kpriya1203@gmail.com>
2024-07-14 21:26:43 +0530

    query setup in gallery

--- 05-UnsplashImages-ReactQuery/src/components/Gallery.jsx
+++ 05-UnsplashImages-ReactQuery/src/components/Gallery.jsx
@@ -1,4 +1,18 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+const url = `https://api.unsplash.com/search/photos?query=cat&client_id=f4NbmCFlA1q65QOE9SdDSL_jpNnxn2uBusmySTfCC_E`;
+
 const Gallery = () => {
+  const { data } = useQuery({
+    queryKey: ['images'],
+    queryFn: async () => {
+      const response = await axios.get(url);
+      return response.data;
+    },
+  });
+
+  console.log(data);
   return <h4>Gallery</h4>;
 };
 

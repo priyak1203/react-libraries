3da0f5492774fa701224d464a3ef3eee70f3d036
Priydarshini Karanth <kpriya1203@gmail.com>
2024-07-16 12:15:24 +0530

    query parameters

--- 05-UnsplashImages-ReactQuery/src/components/Gallery.jsx
+++ 05-UnsplashImages-ReactQuery/src/components/Gallery.jsx
@@ -2,22 +2,16 @@ import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import { useGlobalContext } from '../context';
 
-// const url = `https://api.unsplash.com/search/photos?client_id=${
-//   import.meta.env.VITE_API_KEY
-// }`;
-
-const url = `https://api.unsplash.com/search/photos?`;
+const url = `https://api.unsplash.com/search/photos?client_id=${
+  import.meta.env.VITE_API_KEY
+}`;
 
 const Gallery = () => {
   const { searchTerm } = useGlobalContext();
   const { isError, isLoading, data } = useQuery({
     queryKey: ['images', searchTerm],
     queryFn: async () => {
-      const response = await axios.get(`${url}&query=${searchTerm}`, {
-        headers: {
-          Authorization: `Client-ID ${import.meta.env.VITE_API_KEY}`,
-        },
-      });
+      const response = await axios.get(`${url}&query=${searchTerm}`);
       return response.data;
     },
   });

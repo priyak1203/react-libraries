e447225c275f0d01333050eca5f2bacd99579f55
Priydarshini Karanth <kpriya1203@gmail.com>
2024-07-17 13:02:39 +0530

    fetchProjects custom hook

--- 06-Contentful-CMS/src/fetchProjects.jsx
+++ 06-Contentful-CMS/src/fetchProjects.jsx
@@ -1,4 +1,5 @@
 import { createClient } from 'contentful';
+import { useEffect, useState } from 'react';
 
 const client = createClient({
   space: 'aa2k8rr465mf',
@@ -6,6 +7,33 @@ const client = createClient({
   accessToken: import.meta.env.VITE_ACCESS_TOKEN,
 });
 
-client
-  .getEntries({ content_type: 'projects' })
-  .then((response) => console.log(response));
+export const useFetchProjects = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [projects, setProjects] = useState([]);
+
+  const getData = async () => {
+    try {
+      const response = await client.getEntries({ content_type: 'projects' });
+
+      const projects = response.items.map((item) => {
+        const { title, url, image } = item.fields;
+        const id = item.sys.id;
+        const img_url = image?.fields?.file?.url;
+
+        return { title, url, id, img_url };
+      });
+
+      setProjects(projects);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return { isLoading, projects };
+};

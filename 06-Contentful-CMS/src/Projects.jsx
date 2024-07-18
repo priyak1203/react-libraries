74b09b6508ec42f03742806d75adda6b8e5415dc
Priydarshini Karanth <kpriya1203@gmail.com>
2024-07-18 15:41:32 +0530

    render projects

--- 06-Contentful-CMS/src/Projects.jsx
+++ 06-Contentful-CMS/src/Projects.jsx
@@ -2,8 +2,40 @@ import { useFetchProjects } from './fetchProjects';
 
 const Projects = () => {
   const { isLoading, projects } = useFetchProjects();
-  console.log(isLoading, projects);
-  return <h2>Projects</h2>;
+
+  if (isLoading) {
+    return (
+      <section className="projects">
+        <h2>Loading...</h2>
+      </section>
+    );
+  }
+
+  return (
+    <section className="projects">
+      <div className="title">
+        <h2>projects</h2>
+        <div className="title-underline"></div>
+      </div>
+      <div className="projects-center">
+        {projects.map((project) => {
+          const { id, title, img_url, url } = project;
+          return (
+            <a
+              key={id}
+              href={url}
+              target="_blank"
+              rel="noreferrer"
+              className="project"
+            >
+              <img src={img_url} alt={title} className="img" />
+              <h5>{title}</h5>
+            </a>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Projects;

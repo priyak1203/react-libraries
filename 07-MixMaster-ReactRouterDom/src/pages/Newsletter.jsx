1fd694c1364418dbefd1a071eebfcf3005e18244
Priydarshini Karanth <kpriya1203@gmail.com>
2024-07-23 21:08:14 +0530

    newsletter structure

--- 07-MixMaster-ReactRouterDom/src/pages/Newsletter.jsx
+++ 07-MixMaster-ReactRouterDom/src/pages/Newsletter.jsx
@@ -1,5 +1,41 @@
 const Newsletter = () => {
-  return <h2>Newsletter</h2>;
+  return (
+    <form className="form">
+      <h4>our newsletter</h4>
+      {/* name */}
+      <div className="form-row">
+        <label htmlFor="name" className="form-label">
+          name
+        </label>
+        <input type="text" id="name" name="name" className="form-input" />
+      </div>
+      {/* last name */}
+      <div className="form-row">
+        <label htmlFor="lastName" className="form-label">
+          last name
+        </label>
+        <input
+          type="text"
+          id="lastName"
+          name="lastName"
+          className="form-input"
+        />
+      </div>
+      <div className="form-row">
+        <label htmlFor="email" className="form-label">
+          email
+        </label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          className="form-input"
+          defaultValue="test@test.com"
+        />
+      </div>
+      <button className="btn btn-block">submit</button>
+    </form>
+  );
 };
 
 export default Newsletter;

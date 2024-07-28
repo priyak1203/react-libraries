847bb2ce32e5cc7f42c0a92def303c92d8d1f336
Priydarshini Karanth <kpriya1203@gmail.com>
2024-07-28 21:26:20 +0530

    navbar with styles

--- 08-DevPortfolio-TailwindCSS/src/App.jsx
+++ 08-DevPortfolio-TailwindCSS/src/App.jsx
@@ -1,7 +1,9 @@
+import { Navbar } from './components';
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Tailwind Portfolio</h1>
+      <Navbar />
     </>
   );
 }

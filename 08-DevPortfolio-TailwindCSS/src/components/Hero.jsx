be14d1728d1479f2dd6a3e3645464d71f63eb8c3
Priydarshini Karanth <kpriya1203@gmail.com>
2024-07-29 12:54:15 +0530

    adding custom classes in tailwind

--- 08-DevPortfolio-TailwindCSS/src/components/Hero.jsx
+++ 08-DevPortfolio-TailwindCSS/src/components/Hero.jsx
@@ -4,7 +4,7 @@ import heroImg from '../assets/hero.svg';
 const Hero = () => {
   return (
     <section className="bg-emerald-100 py-24">
-      <div className="mx-auto max-w-7xl px-8 grid md:grid-cols-2 items-center gap-8">
+      <div className="align-element grid md:grid-cols-2 items-center gap-8">
         <article>
           <h1 className="text-7xl font-bold tracking-wider">I'm John</h1>
           <p className="mt-4 text-3xl text-slate-700 capitalize tracking-wide">

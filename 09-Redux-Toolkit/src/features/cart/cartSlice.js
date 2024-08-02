6df278b546ce501cc9f3298f7462129c1dfdd7cf
Priydarshini Karanth <kpriya1203@gmail.com>
2024-08-02 10:56:59 +0530

    calculate totals using reduce function

--- 09-Redux-Toolkit/src/features/cart/cartSlice.js
+++ 09-Redux-Toolkit/src/features/cart/cartSlice.js
@@ -27,11 +27,30 @@ const cartSlice = createSlice({
       const cartItem = state.cartItems.find((item) => item.id === payload.id);
       cartItem.amount = cartItem.amount - 1;
     },
+    calculateTotals: (state) => {
+      let { total, amount } = state.cartItems.reduce(
+        (cartTotal, item) => {
+          const { price, amount } = item;
+          cartTotal.total += amount * price;
+          cartTotal.amount += amount;
+          return cartTotal;
+        },
+        {
+          total: 0,
+          amount: 0,
+        }
+      );
+
+      total = parseFloat(total.toFixed(2));
+      state.amount = amount;
+      state.total = total;
+    },
   },
 });
 
 // console.log(cartSlice);
 
-export const { clearCart, removeItem, increase, decrease } = cartSlice.actions;
+export const { clearCart, removeItem, increase, decrease, calculateTotals } =
+  cartSlice.actions;
 
 export default cartSlice.reducer;

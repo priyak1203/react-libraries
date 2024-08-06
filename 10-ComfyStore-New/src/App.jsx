91cde24af99168f674967250118e0fdbe0b4b05a
Priydarshini Karanth <kpriya1203@gmail.com>
2024-08-06 12:48:31 +0530

    router structure setup

--- 10-ComfyStore-New/src/App.jsx
+++ 10-ComfyStore-New/src/App.jsx
@@ -1,8 +1,71 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  About,
+  Cart,
+  Checkout,
+  Error,
+  HomeLayout,
+  Landing,
+  Login,
+  Orders,
+  Products,
+  Register,
+  SingleProduct,
+} from './pages';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: 'about',
+        element: <About />,
+      },
+      {
+        path: 'products',
+        element: <Products />,
+      },
+      {
+        path: 'products/:id',
+        element: <SingleProduct />,
+      },
+      {
+        path: 'cart',
+        element: <Cart />,
+      },
+      {
+        path: 'checkout',
+        element: <Checkout />,
+      },
+      {
+        path: 'orders',
+        element: <Orders />,
+      },
+    ],
+  },
+  {
+    path: '/register',
+    element: <Register />,
+    errorElement: <Error />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+    errorElement: <Error />,
+  },
+]);
+
 function App() {
   return (
-    <>
+    <RouterProvider router={router}>
       <h1 className="text-3xl font-bold underline">Comfy Store New</h1>
-    </>
+    </RouterProvider>
   );
 }
 
--- 10-ComfyStore-New/src/pages/HomeLayout.jsx
+++ 10-ComfyStore-New/src/pages/HomeLayout.jsx
@@ -1,5 +1,14 @@
+import { Outlet } from 'react-router-dom';
+
 const HomeLayout = () => {
-  return <div>HomeLayout</div>;
+  return (
+    <>
+      <nav>
+        <span className="text-4xl text-primary">Comfy Store New</span>
+      </nav>
+      <Outlet />
+    </>
+  );
 };
 
 export default HomeLayout;

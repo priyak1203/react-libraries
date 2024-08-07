b623837966cfebf6be514445715225c74eb399f8
Priydarshini Karanth <kpriya1203@gmail.com>
2024-08-07 13:03:45 +0530

    form input component

--- 10-ComfyStore-New/src/pages/Login.jsx
+++ 10-ComfyStore-New/src/pages/Login.jsx
@@ -1,5 +1,11 @@
+import { FormInput } from '../components';
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <FormInput type="text" label="username" />
+    </div>
+  );
 };
 
 export default Login;

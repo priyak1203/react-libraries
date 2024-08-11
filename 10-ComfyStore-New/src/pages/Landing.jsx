b9c09b6c375d39402bff53b99d9a1445fc5543f7
Priydarshini Karanth <kpriya1203@gmail.com>
2024-08-11 12:02:33 +0530

    landing page data fetch with loader

--- 10-ComfyStore-New/src/pages/Landing.jsx
+++ 10-ComfyStore-New/src/pages/Landing.jsx
@@ -1,4 +1,19 @@
 import { Hero } from '../components';
+import { customFetch } from '../utils';
+
+const url = '/products?featured=true';
+
+export const loader = async () => {
+  try {
+    const response = await customFetch(url);
+    console.log(response);
+    const products = response.data.data;
+    return products;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
 
 const Landing = () => {
   return (

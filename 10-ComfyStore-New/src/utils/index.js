822e2fde1472344e6fe8a96d0fb564079425a8e2
Priydarshini Karanth <kpriya1203@gmail.com>
2024-08-12 11:01:32 +0530

    format price function

--- 10-ComfyStore-New/src/utils/index.js
+++ 10-ComfyStore-New/src/utils/index.js
@@ -5,3 +5,12 @@ const productionURL = 'https://strapi-store-server.onrender.com/api';
 export const customFetch = axios.create({
   baseURL: productionURL,
 });
+
+export const formatPrice = (price) => {
+  const dollarsAmount = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  }).format((price / 100).toFixed(2));
+
+  return dollarsAmount;
+};

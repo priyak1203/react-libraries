9d04f12dcde2d29c14f820065fd0b47ae6b618f8
Priydarshini Karanth <kpriya1203@gmail.com>
2024-08-22 13:03:46 +0530

    refactor toggle theme

--- 10-ComfyStore-New/src/features/user/userSlice.js
+++ 10-ComfyStore-New/src/features/user/userSlice.js
@@ -1,8 +1,19 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const themes = {
+  winter: 'winter',
+  dracula: 'dracula',
+};
+
+const getThemeFromLocalStorage = () => {
+  const theme = localStorage.getItem('theme') || themes.winter;
+  document.documentElement.setAttribute('data-theme', theme);
+  return theme;
+};
+
 const initialState = {
   user: { username: 'coding addict' },
-  theme: 'dracula',
+  theme: getThemeFromLocalStorage(),
 };
 
 const userSlice = createSlice({
@@ -16,7 +27,10 @@ const userSlice = createSlice({
       console.log('logout user');
     },
     toggleTheme: (state) => {
-      console.log('toggle theme');
+      const { winter, dracula } = themes;
+      state.theme = state.theme === dracula ? winter : dracula;
+      document.documentElement.setAttribute('data-theme', state.theme);
+      localStorage.setItem('theme', state.theme);
     },
   },
 });

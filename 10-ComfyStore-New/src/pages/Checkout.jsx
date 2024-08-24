4b6b70cff33f6a5a0b71db6ee304f02aa1636937
Priydarshini Karanth <kpriya1203@gmail.com>
2024-08-24 13:37:49 +0530

    checkout page setup

--- 10-ComfyStore-New/src/pages/Checkout.jsx
+++ 10-ComfyStore-New/src/pages/Checkout.jsx
@@ -1,5 +1,22 @@
+import { CartTotals, CheckoutForm, SectionTitle } from '../components';
+import { useSelector } from 'react-redux';
+
 const Checkout = () => {
-  return <div>Checkout</div>;
+  const cartItems = useSelector((state) => state.cartState.cartItems);
+
+  if (cartItems.length === 0) {
+    return <SectionTitle text="your cart is empty" />;
+  }
+
+  return (
+    <>
+      <SectionTitle text="place your order" />
+      <div className="mt-8 grid gap-8 md:grid-cols-2 items-start">
+        <CheckoutForm />
+        <CartTotals />
+      </div>
+    </>
+  );
 };
 
 export default Checkout;

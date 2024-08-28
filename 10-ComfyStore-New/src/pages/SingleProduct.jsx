cf5b86511a1cc4e15f1edfe3fd86c6a128b8964c
Priydarshini Karanth <kpriya1203@gmail.com>
2024-08-28 20:18:05 +0530

    caching single product requests

--- 10-ComfyStore-New/src/pages/SingleProduct.jsx
+++ 10-ComfyStore-New/src/pages/SingleProduct.jsx
@@ -4,11 +4,20 @@ import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { addItem } from '../features/cart/cartSlice';
 
+const singleProductQuery = (id) => {
+  return {
+    queryKey: ['singleProduct', id],
+    queryFn: () => customFetch(`/products/${id}`),
+  };
+};
+
 export const loader =
   (queryClient) =>
   async ({ params }) => {
     try {
-      const response = await customFetch(`/products/${params.id}`);
+      const response = await queryClient.ensureQueryData(
+        singleProductQuery(params.id)
+      );
       return { product: response.data.data };
     } catch (error) {
       console.log(error);

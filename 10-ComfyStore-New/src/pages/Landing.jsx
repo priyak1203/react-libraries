3299e0d41fda09ac9e3d17667d8085dca5182f52
Priydarshini Karanth <kpriya1203@gmail.com>
2024-08-28 20:11:55 +0530

    fetching data using react query in landing page

--- 10-ComfyStore-New/src/pages/Landing.jsx
+++ 10-ComfyStore-New/src/pages/Landing.jsx
@@ -3,9 +3,14 @@ import { customFetch } from '../utils';
 
 const url = '/products?featured=true';
 
+const featuredProductsQuery = {
+  queryKey: ['featuredProducts'],
+  queryFn: () => customFetch(url),
+};
+
 export const loader = (queryClient) => async () => {
   try {
-    const response = await customFetch(url);
+    const response = await queryClient.ensureQueryData(featuredProductsQuery);
     const products = response.data.data;
     return { products };
   } catch (error) {

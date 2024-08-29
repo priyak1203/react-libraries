eeb66a7099ef896d4f9e3489a94ab6d3b4efa00d
Priydarshini Karanth <kpriya1203@gmail.com>
2024-08-29 13:53:07 +0530

    orders query caching fix

--- 10-ComfyStore-New/src/components/CheckoutForm.jsx
+++ 10-ComfyStore-New/src/components/CheckoutForm.jsx
@@ -33,6 +33,7 @@ export const action =
           },
         }
       );
+      queryClient.removeQueries(['orders']);
       store.dispatch(clearCart());
       toast.success('order placed successfully');
       return redirect('/orders');

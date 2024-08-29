25132bac1e37d75197e2df61cfd0f28804f1ffd4
Priydarshini Karanth <kpriya1203@gmail.com>
2024-08-29 13:17:43 +0530

    query caching in all products page

--- 10-ComfyStore-New/src/pages/Products.jsx
+++ 10-ComfyStore-New/src/pages/Products.jsx
@@ -3,6 +3,24 @@ import { customFetch } from '../utils';
 
 const url = '/products';
 
+const allProductsQuery = (queryParams) => {
+  const { search, category, company, sort, price, shipping, page } =
+    queryParams;
+  return {
+    queryKey: [
+      'products',
+      search ?? '',
+      category ?? 'all',
+      company ?? 'all',
+      sort ?? 'a-z',
+      price ?? 100000,
+      shipping ?? false,
+      page ?? 1,
+    ],
+    queryFn: () => customFetch(url, { params: queryParams }),
+  };
+};
+
 export const loader =
   (queryClient) =>
   async ({ request }) => {
@@ -11,7 +29,9 @@ export const loader =
     ]);
 
     try {
-      const response = await customFetch(url, { params });
+      const response = await queryClient.ensureQueryData(
+        allProductsQuery(params)
+      );
       const products = response.data.data;
       const meta = response.data.meta;
       return { products, meta, params };
